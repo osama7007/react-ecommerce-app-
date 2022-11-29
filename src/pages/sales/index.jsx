c7f47f52389bb8fa477c514f8edf7402d4feb8d5
osama7007 <saadosama43@gmail.com>
2022-11-29 15:48:36 +0200

    update sales

--- src/pages/sales/index.jsx
+++ src/pages/sales/index.jsx
@@ -1,38 +1,39 @@
-import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { motion } from "framer-motion";
 import ProductCard from "../../components/producrCard";
-
+import HeadLine from '../../components/headLine';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchProducts } from '../../redux/productSlice';
 const Sales = () => {
-    const [sales, setSales] = useState([]);
-const navigate = useNavigate();
-const getSale = ()=>{
-    fetch(`http://localhost:3005/products?sale=true`)
-    .then(res => res.json())
-    .then(json=> setSales(json))
-}
-
-useEffect(()=>{
-    getSale();
-},[]);
-
-const animations = {
-    initial: { scale: 0 },
-    animate: { scale: 1 },
-    exit: { scale: 0 },
-    transition: { delay: 0.5 },
-    duration: 5,
-  }
+    const {records} = useSelector(state=> state.product)
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchProducts());
+      }, [])
+    const animations = {
+        initial: { scale: 0 },
+        animate: { scale: 1 },
+        exit: { scale: 0 },
+        transition: { delay: 0.5 },
+        duration: 5,
+      }
   return (
     <motion.div {...animations}>    
+        <HeadLine title={"Sales"}/> 
     <div className="d-flex flex-wrap justify-content-center container-fluid">
-            {
-            sales.map((product)=>{
+    {
+             records.filter((product)=>{
                 return(
-                    <ProductCard product={product} navigation={()=>navigate(`/product/${product.id}`)} key={product.id}/>
+                    product.sale === true
                 )
-            })
-    }
+            }).map(product =>{
+                return (
+                       <ProductCard product={product} navigation={()=>navigate(`/product/${product.id}`)} key={product.id}/>
+                )
+            }
+    )}
         </div>
         </motion.div>
   )
